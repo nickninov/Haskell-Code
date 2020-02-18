ca68e229b89a4ba8c31f3c13cc0de09a045e3013
Nick Ninov <46753183+nickninov@users.noreply.github.com>
2020-02-18 00:38:12 +0000

    Haskell improvements of exercise 3
    A more detailed approach to exercises 3 from week 4

--- week4.hs
+++ week4.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- 2. Write a program that provides information about  
 -- a person’s weight using your BMI program
 -- and the following conditions:
@@ -27,21 +29,52 @@ bmi w
 -- Hint1: You can use Monday = 1 to Sunday = 7
 -- Hint2: You can use 0 for 12AM and 2359 for 11:59PM
 
+tflInfo :: [Char] -> [Char] -> Integer -> [Char]
 tflInfo payment day time 
-    | payment == "cash" = "Cash payment costs 4.70 during any time"
-    | payment == "card" = getDay day time
+    | toLowerString payment == "cash" = "Cash payment costs 4.70 during any time"
+    | toLowerString payment == "card" = getDay day time
     | otherwise = "N/A"
 
+getDay :: [Char] -> Integer -> [Char]
 getDay day time
-    | day >= 1 && day <= 5 = getTime time
-    | day == 6 || day == 7 = "Oyster costs 1.50"
+    | toLowerString day == "monday" || toLowerString day == "tuesday" || toLowerString day == "wednesday" || toLowerString day == "thursday" || toLowerString day == "friday" = getTime time
+    | toLowerString day == "saturday" || toLowerString day == "sunday" = "Oyster costs 1.50"
     | otherwise = "N/A"
 
-getTime time
-    | time >= 630 && time <= 0930 = "Oyster costs 1.60"
-    | time >= 1600 && time <= 1900 = "Oyster costs 1.60"
+getTime :: Integer -> [Char]
+getTime time 
+    | (getHour time >= 6 && getHour time <= 9) && (getMin time >= 30 && getMin time <= 59) = "Monday to Friday - 06:30 - 09:30 - Oyster costs 1.60"
+    | (getHour time >= 16 && getHour time <= 19) && (getMin time >= 0 && getMin time <= 59) = "Monday to Friday - 16:00 - 19:00 - Oyster costs 1.60"
+    | getHour time > 23 || getMin time > 59 = "Given time does not exist... "
     | otherwise = "Oyster costs 1.50"
 
+-- Get the hour from a number
+getHour :: Integer -> Int
+getHour time 
+    | numLength time == 3 = do
+        let num = read (take 1 (show time)) :: Int
+        num
+    | numLength time == 4 = do
+        let num = read (take 2 (show time)) :: Int
+        num
+
+-- Get minutes from a number
+getMin :: Show a => a -> Int
+getMin time 
+    | length (drop 2 (show time)) == 1 = do
+        let num = read (drop 1 (show time)) :: Int
+        num
+    | length (drop 2 (show time)) == 2 = do
+        let num = read (drop 2 (show time)) :: Int
+        num
+
+-- Get a number's length
+numLength :: Show a => a -> Int
+numLength num = length (show num)
+
+-- Make string to lower case
+toLowerString :: [Char] -> [Char]
+toLowerString chars = [toLower x | x <- chars]
 
 -- 4. For numbers ranging from 1 to 100 (inclusive): 
 -- print “foo” if the number is divisible by 3,
